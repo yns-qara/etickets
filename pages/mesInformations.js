a371b80c725b739651d863bae4d9ac68416ca0c8
yns-qara <younesqara@gmail.com>
2022-09-03 20:27:37 +0100

    commit

--- pages/mesInformations.js
+++ pages/mesInformations.js
@@ -2,16 +2,20 @@ import Navbar from "../components/Navbar2"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import Input from "../components/Input"
+import infoHolder from "../components/infoHolder"
 const mesInformations = props => {
     return (
         <>
             {/* <Navbar /> */}
+            <infoHolder>
             <Input
                 label="name"
                 type="text"
                 ID = "inpt"
                 placeholder="wassim"
             />
+            </infoHolder>
+
             {/* <Footer /> */}
         </>
     )
