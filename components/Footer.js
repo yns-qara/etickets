c0fc39660c559e5574b2bb9dfa84a42b2b651e46
yns-qara <younesqara@gmail.com>
2022-09-16 01:30:36 +0100

    1:30

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 import styles from '../styles/footer.module.css'
 import Envoloppe from './SVGs/envolope'
-import Logof from './SVGs/logoFooter'
+import LogoT from './SVGs/logoT'
 import Apple from './SVGs/apple'
 import Android from './SVGs/android'
 import Messenger from './SVGs/messenger'
@@ -24,7 +24,7 @@ const Footer = props => {
             <div className={styles.middle__footer}>
 
                 <div className={styles.left_middle}>
-                    <Logof />
+                    <LogoT />
                     <p>Un site de billetterie nouvelle génération vous offrant la possibilité d’acheter en ligne des tickets pour les événements de votre choix. Sa vocation est de vous proposer l’offre de concerts, sports, spectacles, cinéma, festivals, théâtre, humour, voyages et evasions la plus large du Maroc en collaboration avec les organisateurs d’événements.</p>
                     <h3>NOS APPLICATIONS</h3>
                     <div className={styles.buttonHolder}>
---
+++ pages/ticket.js
@@ -0,0 +1,73 @@
+import Nav from '../components/Navbar2'
+import Footer from '../components/Footer'
+import styles from '../styles/ticketInfos.module.css'
+import LogoT from '../components/SVGs/logoT'
+import Download from '../components/SVGs/download'
+const ticket = props => {
+    return (
+        <>
+            <Nav />
+            <div className={styles.container}>
+
+                <div className={styles.title}>
+                    <LogoT />
+                </div>
+                <div className={styles.corp}>
+                    <div className={styles.detailes}>
+                        <h1>Confirmation de votre commande</h1>
+                        <p>Le 26/05/2022</p>
+                        <p>Identifiant: <span>2402</span></p>
+                        <p>Madame, Monsieur: <span>MOUAD DAHMANI</span></p>
+                        <p>N° Commande: <span>205304</span></p>
+                        <p>Montant: <span>200,00 MAD</span></p>
+                        <p>Montant des places: <span>2</span></p>
+                        <h5>Nous vous confirmons votre achat de places au nom de DAHMANI, sur notre site internet</h5>
+                    </div>
+                    <div className={styles.table}>
+                        <div className={styles.header}>MATCH</div>
+                        <div className={styles.header}>LIEU</div>
+                        <div className={styles.header}>DATE</div>
+                        <div className={styles.header}>CLASSE</div>
+                        <div className={styles.header}>PRIX</div>
+                        <div>MATCH FINALE AL AHLY SC - WAC AC</div>
+                        <div>COMPLEXE SPORTIF
+                            MOHAMMED V CASABLANCA</div>
+                        <div>30/05/2022 20:00</div>
+                        <div>CATEGORIE
+                            1</div>
+                        <div>100,00 DHS</div>
+                        <div>MATCH FINALE AL AHLY SC - WAC AC</div>
+                        <div>COMPLEXE SPORTIF
+                            MOHAMMED V CASABLANCA</div>
+                        <div>30/05/2022 20:00</div>
+                        <div>CATEGORIE
+                            1</div>
+                        <div>100,00 DHS</div>
+                        <div className={styles.span3}>Montant des places </div>
+                        <div>2</div>
+                        <div>200,00 DHS</div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div className={styles.span4}>MONTANT TOTAL DU PANIER </div>
+                        <div>200,00 DHS</div>
+                    </div>
+                    <div className={styles.qrCodeBut}>
+                        <div></div>
+                        <button>
+                            <span>DOWNLOAD</span>
+                            <Download />
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+
+        </>
+    )
+}
+
+
+export default ticket
