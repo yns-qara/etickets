3f14c9b7330a2f440e91c550351bca15d89dca18
yns-qara <younesqara@gmail.com>
2022-09-24 01:13:11 +0100

    fixing the dependency bug

--- components/CountDownDL.jsx
+++ components/CountDownDL.jsx
@@ -74,7 +74,7 @@ const CountDownDL = ({ name, day, month ,dateExpired , setDateExpired}) => {
         }));
       }
     }, 1000);
-  }, [currentYear, day, isItBday, month , dateExpired , setDateExpired]);
+  }, [currentYear, day, isItBday, month]);
 
 
   return (
