3f087e888b2c75f92670e329dc289e2cf2c0850e
yns-qara <younesqara@gmail.com>
2022-11-02 21:12:14 +0100

    😍 reserve place

--- components/Launch.js
+++ components/Launch.js
@@ -1,17 +1,79 @@
 import styles from '../styles/launch.module.css'
-import Image from 'next/image'
+import Woman from './SVGs/Woman'
+import { useState } from 'react'
+import LaunchLogo from './SVGs/LaunchLogo';
 const Launch = () => {
+
+
+    const [email, setEmail] = useState('');
+    const [nom, setNom] = useState('');
+    const [prenom, setPrenom] = useState('');
+
+    const [success, setSuccess] = useState(false)
+
+    const handleSignUp = async () => {
+        setSuccess(true);
+        const response = await fetch('https://6340100ad1fcddf69cb045a9.mockapi.io/api/v1/emails', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nom: nom,
+                prenom: prenom,
+                email: email
+            })
+        });
+        return response.json();
+    }
+
 
 
     return (
         <>
-            <div className={styles.container}>
-                <h1>ouverture prochaine. <span>a tres vite</span></h1>
 
-                <div>
-                    <Image src="/etic.jpg" width="750" height="600" />
+
+            <div className={styles.container}>
+                <div className={styles.logo}>
+                    <LaunchLogo />
+                </div>
+
+                <h1>Plusieurs nouvelles arrivent très bientôt</h1>
+                <p>Entrez votre adresse e-mail pour être au courant en premier et bénéficiez de nos offres incroyables!</p>
+
+                <form
+                    className={styles.form}
+                    onSubmit={handleSignUp}
+                >
+                    <div>
+                        <input
+                            type="text"
+                            onChange={(e) => setNom(e.target.value)}
+                            placeholder='Nom'
+                            required
+                        />
+                        <input
+                            type="text"
+                            onChange={(e) => setPrenom(e.target.value)}
+                            placeholder='Prenom'
+                            required
+                        />
+                    </div>
+                    <input
+                        type="text"
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder='Email'
+                        required
+                    />
+                    <button type='submit'>Envoyer</button>
+                </form>
+
+                <div className={styles.woman}>
+                    <Woman />
                 </div>
             </div>
+            <footer className={styles.footer}></footer>
+
         </>
     )
 }
