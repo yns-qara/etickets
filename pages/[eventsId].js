db192f5222f40757b14f0f59835fb2355a6ecaa8
yns-qara <younesqara@gmail.com>
2022-11-02 19:19:50 +0100

    😍 launch 2

--- pages/[eventsId].js
+++ pages/[eventsId].js
@@ -1,64 +1,64 @@
-// import Navbar from "../components/Navbar2"
-// import Footer from "../components/Footer"
-// import JachetHead from '../components/JachetHead'
-// import CardJachete from "../components/CardJachete"
-// import BodyJachete from "../components/BodyJachete"
-// import { motion} from 'framer-motion'
+import Navbar from "../components/Navbar2"
+import Footer from "../components/Footer"
+import JachetHead from '../components/JachetHead'
+import CardJachete from "../components/CardJachete"
+import BodyJachete from "../components/BodyJachete"
+import { motion} from 'framer-motion'
 
 
 
-// export const getServerSideProps = async (context) => {
-//   const {params} = context;
-//   const res = await fetch(`https://6340100ad1fcddf69cb045a9.mockapi.io/api/v1/events/${params.eventsId}`);
-//   const data = await res.json();
-//   return {
-//     props: {
-//       evenement: data
-//     }
-//   }
-// }
+export const getServerSideProps = async (context) => {
+  const {params} = context;
+  const res = await fetch(`https://6340100ad1fcddf69cb045a9.mockapi.io/api/v1/events/${params.eventsId}`);
+  const data = await res.json();
+  return {
+    props: {
+      evenement: data
+    }
+  }
+}
 
 
-// const Jachete = ({evenement}) => {
+const Jachete = ({evenement}) => {
 
 
-//   return (
-//     <>
-//       <animatePresense>
+  return (
+    <>
+      <animatePresense>
 
-//         <Navbar />
+        <Navbar />
         
-//         <motion.div
-//           initial={{ y: 150, opacity: 0 }}
-//           animate={{ y: 0, opacity: 1 }}
-//           exit={{ opacity: 0 }}
-//           transition={{ duration: .8, ease: 'easeInOut' }}
-//         >
-//           <JachetHead title={evenement.title}>
-//             <CardJachete 
-//                title={evenement.title}
-//                date={evenement.date}
-//                lieu={evenement.lieu}
-//                month={evenement.month}
-//                day={evenement.day}
-//                prix={evenement.price}
-//                city={evenement.city}
-//                imgURL={evenement.url}
-//                detailsImg={evenement.detailsImg}
-//             />
-//           </JachetHead>
+        <motion.div
+          initial={{ y: 150, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: .8, ease: 'easeInOut' }}
+        >
+          <JachetHead title={evenement.title}>
+            <CardJachete 
+               title={evenement.title}
+               date={evenement.date}
+               lieu={evenement.lieu}
+               month={evenement.month}
+               day={evenement.day}
+               prix={evenement.price}
+               city={evenement.city}
+               imgURL={evenement.url}
+               detailsImg={evenement.detailsImg}
+            />
+          </JachetHead>
           
-//           <BodyJachete description={evenement.description} />
+          <BodyJachete description={evenement.description} />
 
-//           <Footer />
+          <Footer />
 
-//         </motion.div>
-//       </animatePresense>
+        </motion.div>
+      </animatePresense>
 
-//     </>
+    </>
 
-//   )
-// }
+  )
+}
 
 
-// export default Jachete
+export default Jachete
